2bc2c2aeea3a6ddf290a7c3ac1661e305a36c935
Furitsuri <123718338+Furitsuri@users.noreply.github.com>
2025-05-13 17:29:17 +0900

    FocusRoutineの基礎処理を追加

---
+++ Assets/Game/Scripts/Interfaces/IFocusRoutine.cs
@@ -0,0 +1,12 @@
+﻿public interface IFocusRoutine
+{
+    public void Update();
+
+    public void Restart();
+
+    public void Exit();
+
+    public bool IsRunning();
+
+    public int GetPriority() { return (int)FocusRoutinePriority.NONE; }
+}
