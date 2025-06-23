dc4ceb459d41e8092f44a023a9b849e26a819296
Furitsuri <123718338+Furitsuri@users.noreply.github.com>
2025-06-23 17:26:05 +0900

    InstallerとUiSystemのインターフェイスを追加

---
+++ Assets/Game/Scripts/Interfaces/IInstaller.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IInstaller
+{
+    public void InstallBindings();
+
+    public void InstallBindings<T>(T instance);
+}
---
+++ Assets/Game/Scripts/Interfaces/IUiSystem.cs
@@ -0,0 +1,15 @@
+﻿using Frontier;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public interface IUiSystem
+{
+    public GeneralUISystem GeneralUi { get; }
+    public BattleUISystem BattleUi { get; }
+#if UNITY_EDITOR
+    public DebugUISystem DebugUi { get; }
+#endif // UNITY_EDITOR
+    public void InitializeUiSystem();
+}
