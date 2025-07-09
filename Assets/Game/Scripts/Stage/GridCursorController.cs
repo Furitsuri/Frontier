b7266898d8ed8c4f778b3b80db29dd8e262a32c5
Furitsuri <123718338+Furitsuri@users.noreply.github.com>
2025-07-09 18:19:04 +0900

    ステージ編集用のソースコードを追加

---
+++ Assets/Game/Scripts/Stage/GridCursorController.cs
@@ -0,0 +1,272 @@
+﻿using Frontier.Entities;
+using UnityEngine;
+using static Constants;
+
+#pragma warning disable 0618
+
+namespace Frontier.Stage
+{
+    public class GridCursorController : MonoBehaviour
+    {
+        /// <summary>
+        /// グリッドカーソルの状態
+        /// </summary>
+        public enum State
+        {
+            NONE = 0,
+            MOVE,
+            ATTACK
+        }
+
+        [Header("移動補間時間")]
+        [SerializeField]
+        private float MoveInterpolationTime = 1f;
+
+        private LineRenderer _lineRenderer;
+        private StageData _stageData    = null;
+        private Vector3 _beginPos       = Vector3.zero;
+        private Vector3 _endPos         = Vector3.zero;
+        private Vector3 _currentPos     = Vector3.zero;
+        private int _atkTargetIndex     = 0;
+        private int _atkTargetNum       = 0;
+        private float _totalTime        = 0;
+
+        public int Index { get; set; } = 0;
+        public State GridState { get; set; } = State.NONE;
+        public Character BindCharacter { get; set; } = null;
+
+        private void Start()
+        {
+            _lineRenderer = gameObject.GetComponent<LineRenderer>();
+        }
+
+        /// <summary>
+        /// 初期化します
+        /// </summary>
+        /// <param name="initIndex">初期インデックス値</param>
+        /// <param name="rowNum">盤面における行に該当するグリッド数</param>
+        /// <param name="columnNum">盤面における列に該当するグリッド数</param>
+        public void Init(int initIndex, StageData stageData)
+        {
+            Index           = initIndex;
+            _stageData      = stageData;
+            _atkTargetIndex = 0;
+            _atkTargetNum   = 0;
+            GridState       = State.NONE;
+            BindCharacter   = null;
+        }
+
+        /// <summary>
+        /// インデックス値を現在グリッドの上に該当する値に設定します
+        /// </summary>
+        public void Up()
+        {
+            var rowNum = _stageData.GridRowNum;
+
+            StartLerpMove();
+            Index += rowNum;
+            if (rowNum * rowNum <= Index)
+            {
+                Index = Index % (rowNum * rowNum);
+            }
+        }
+
+        /// <summary>
+        /// インデックス値を現在グリッドの下に該当する値に設定します
+        /// </summary>
+        public void Down()
+        {
+            var rowNum = _stageData.GridRowNum;
+
+            StartLerpMove();
+            Index -= rowNum;
+            if (Index < 0)
+            {
+                Index += rowNum * rowNum;
+            }
+        }
+
+        /// <summary>
+        /// インデックス値を現在グリッドの右に該当する値に設定します
+        /// </summary>
+        public void Right()
+        {
+            var rowNum = _stageData.GridRowNum;
+
+            StartLerpMove();
+            Index++;
+            if (Index % rowNum == 0)
+            {
+                Index -= rowNum;
+            }
+        }
+
+        /// <summary>
+        /// インデックス値を現在グリッドの左に該当する値に設定します
+        /// </summary>
+        public void Left()
+        {
+            var rowNum = _stageData.GridRowNum;
+
+            StartLerpMove();
+            Index--;
+            if ((Index + 1) % rowNum == 0)
+            {
+                Index += rowNum;
+            }
+        }
+
+        /// <summary>
+        /// 攻撃対象インデックス値を取得します
+        /// </summary>
+        /// <returns>攻撃対象インデックス値</returns>
+        public int GetAtkTargetIndex()
+        {
+            return _atkTargetIndex;
+        }
+
+        /// <summary>
+        /// オブジェクトのアクティブ・非アクティブを設定します
+        /// </summary>
+        /// <param name="isActive">アクティブ設定</param>
+        public void SetActive(bool isActive)
+        {
+            gameObject.SetActive(isActive);
+        }
+
+        /// <summary>
+        /// 攻撃対象インデックス値を設定します
+        /// </summary>
+        /// <param name="index">攻撃対象インデックス値</param>
+        public void SetAtkTargetIndex(int index)
+        {
+            _atkTargetIndex = index;
+        }
+
+        /// <summary>
+        /// 攻撃対象インデックスの総数を設定します
+        /// </summary>
+        /// <param name="num">攻撃対象インデックスの総数</param>
+        public void SetAtkTargetNum(int num)
+        {
+            _atkTargetNum = num;
+        }
+
+        /// <summary>
+        /// 次のターゲットインデックス値に遷移します
+        /// </summary>
+        public void TransitNextTarget()
+        {
+            _atkTargetIndex = (_atkTargetIndex + 1) % _atkTargetNum;
+        }
+
+        /// <summary>
+        /// 前のターゲットインデックス値に遷移します
+        /// </summary>
+        public void TransitPrevTarget()
+        {
+            _atkTargetIndex = (_atkTargetIndex - 1) < 0 ? _atkTargetNum - 1 : _atkTargetIndex - 1;
+        }
+
+        /// <summary>
+        /// 攻撃対象情報をクリアします
+        /// </summary>
+        public void ClearAtkTargetInfo()
+        {
+            _atkTargetIndex = 0;
+            _atkTargetNum = 0;
+        }
+
+        public int X()
+        {
+            return Index % _stageData.GridRowNum;
+        }
+
+        public int Y()
+        {
+            return Index / _stageData.GridRowNum;
+        }
+
+        public Vector3 GetPosition()
+        {
+            return _currentPos;
+        }
+
+        private void Update()
+        {
+            UpdateUI( Time.deltaTime );
+        }
+
+        /// <summary>
+        /// 選択しているカーソル位置を更新します
+        /// </summary>
+        /// /// <param name="delta">フレーム間の時間</param>
+        void UpdateUI( float delta )
+        {
+            _endPos = GetGoalPosition();
+
+            if ( GridState == State.NONE || GridState == State.MOVE)
+            {
+                UpdateLerpPosition(delta);
+            }
+            else
+            {
+                DrawSquareLine(TILE_SIZE, _endPos);
+            }
+        }
+
+        /// <summary>
+        /// グリッドの位置を線形補間で更新します
+        /// </summary>
+        /// <param name="delta">フレーム間の時間</param>
+        void UpdateLerpPosition( float delta )
+        {
+            _totalTime += delta;
+            _currentPos = Vector3.Lerp( _beginPos, _endPos, _totalTime / MoveInterpolationTime);
+
+            DrawSquareLine(TILE_SIZE, _currentPos);
+        }
+
+        /// <summary>
+        /// 指定した位置(centralPos)に四角形ラインを描画します
+        /// </summary>
+        /// <param name="gridSize">1グリッドのサイズ</param>
+        /// <param name="centralPos">指定グリッドの中心位置</param>
+        void DrawSquareLine(float gridSize, in Vector3 centralPos)
+        {
+            float halfSize = 0.5f * gridSize;
+
+            Vector3 tileScale = _stageData.GetTile(Index).GetTileScale();
+
+            Vector3[] linePoints = new Vector3[]
+            {
+                new Vector3(-halfSize, tileScale.y, -halfSize) + centralPos,
+                new Vector3(-halfSize, tileScale.y,  halfSize) + centralPos,
+                new Vector3( halfSize, tileScale.y,  halfSize) + centralPos,
+                new Vector3( halfSize, tileScale.y, -halfSize) + centralPos,
+            };
+
+            // SetVertexCountは廃止されているはずだが、使用しないと正常に描画されなかったため使用(2023/5/26)
+            _lineRenderer.SetVertexCount(linePoints.Length);
+            _lineRenderer.SetPositions(linePoints);
+        }
+
+        /// <summary>
+        /// 線形補間移動を開始します
+        /// </summary>
+        private void StartLerpMove()
+        {
+            _beginPos   = GetGoalPosition();
+            _totalTime  = 0f;
+        }
+
+        /// <summary>
+        /// グリッドの現在座標を取得します
+        /// </summary>
+        /// <returns>グリッドの現在座標</returns>
+        private Vector3 GetGoalPosition()
+        {
+            return _stageData.GetTileInfo(Index).charaStandPos;
+        }
+    }
+}
---
+++ Assets/Game/Scripts/Utility/HierarchyBuilderBase.cs
@@ -0,0 +1,231 @@
+﻿using Frontier.Entities;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Zenject;
+
+public class HierarchyBuilderBase : MonoBehaviour
+{
+    // オブジェクト生成クラス
+    protected Generator _generator = null;
+
+    /// <summary>
+    /// DiInstallerから呼び出し、コンテナを登録します
+    /// </summary>
+    /// <param name="container">DIコンテナ</param>
+    [Inject]
+    void Construct(DiContainer container, IInstaller installer)
+    {
+        if (_generator == null)
+        {
+            _generator = gameObject.AddComponent<Generator>();
+        }
+
+        _generator.Inject(container, installer);
+    }
+
+    /// <summary>
+    /// オブジェクト及びコンポーネントを作成し、ヒエラルキー上の任意のオブジェクトの階層下に設置します
+    /// </summary>
+    /// <typeparam name="T">作成するコンポーネントの型</typeparam>
+    /// <param name="initActive">作成したオブジェクトの初期の有効・無効状態</param>
+    /// <returns>作成したコンポーネント</returns>
+    public T CreateComponentAndOrganize<T>(bool initActive, string objName) where T : Behaviour
+    {
+        T generateCpt = _generator.GenerateObjectAndAddComponent<T>(initActive, objName);
+        NullCheck.AssertNotNull(generateCpt);
+
+        Organize(generateCpt);
+
+        return generateCpt;
+    }
+
+    /// <summary>
+    /// 引数に渡したオブジェクトからコンポーネントを作成し、ヒエラルキー上の任意のオブジェクトの階層下に設置します
+    /// </summary>
+    /// <typeparam name="T">作成するコンポーネントの型</typeparam>
+    /// <param name="gameObject">コンポーネントの元となるゲームオブジェクト</param>
+    /// <param name="initActive">ゲームオブジェクトの初期の有効・無効状態</param>
+    /// <returns>作成したコンポーネント</returns>
+    public T CreateComponentAndOrganize<T>(GameObject gameObject, bool initActive) where T : Behaviour
+    {
+        T generateCpt = _generator.GenerateComponentFromObject<T>(gameObject, initActive);
+        NullCheck.AssertNotNull(generateCpt);
+
+        Organize(generateCpt);
+
+        return generateCpt;
+    }
+
+    /// <summary>
+    /// 引数に渡したオブジェクトからコンポーネントを作成します
+    /// また、作成したコンポーネントを親とするオブジェクトの子として設定し、ヒエラルキー上の任意のオブジェクトの階層下に設置します
+    /// </summary>
+    /// <typeparam name="T">作成するコンポーネントの型</typeparam>
+    /// <param name="gameObject">コンポーネントの元となるゲームオブジェクト</param>
+    /// <param name="parentObject">ヒエラルキー上で作成したオブジェクトの親となるオブジェクト</param>
+    /// <param name="initActive">ゲームオブジェクトの初期の有効・無効状態</param>
+    /// <returns>作成したコンポーネント</returns>
+    public T CreateComponentWithNestedParent<T>(GameObject gameObject, GameObject parentObject, bool initActive) where T : Behaviour
+    {
+        T generateCpt = _generator.GenerateComponentFromObject<T>(gameObject, initActive);
+        NullCheck.AssertNotNull(generateCpt);
+
+        generateCpt.transform.SetParent(parentObject.transform, false);
+
+        return generateCpt;
+    }
+
+    /// <summary>
+    /// 引数に渡したオブジェクトからコンポーネントを作成します
+    /// また、作成したコンポーネントを親とするオブジェクトの子として設定し、更に指定の名前で作成したディレクトリの子としてその親を設定の上、
+    /// ヒエラルキー上の任意のオブジェクトの階層下に設置します
+    /// </summary>
+    /// <typeparam name="T">作成するコンポーネントの型</typeparam>
+    /// <param name="gameObject">コンポーネントの元となるゲームオブジェクト</param>
+    /// <param name="parentObject">ヒエラルキー上で作成したオブジェクトの親となるオブジェクト</param>
+    /// <param name="newDirectoryObjectName">作成するヒエラルキー上のオブジェクト(ディレクトリの代替となる空オブジェクト)の名前</param>
+    /// <param name="initActive">ゲームオブジェクトの初期の有効・無効状態</param>
+    /// <returns>作成したコンポーネント</returns>
+    public T CreateComponentWithNestedNewDirectory<T>(GameObject gameObject, GameObject parentObject, string newDirectoryObjectName, bool initActive) where T : Behaviour
+    {
+        T generateCpt = _generator.GenerateComponentFromObject<T>(gameObject, initActive);
+        NullCheck.AssertNotNull(generateCpt);
+
+        GameObject folderObject = new GameObject(newDirectoryObjectName);
+        folderObject.transform.SetParent(parentObject.transform, false);
+        generateCpt.transform.SetParent(folderObject.transform, false);
+
+        return generateCpt;
+    }
+
+    /// <summary>
+    /// DIコンテナを用いてオブジェクト及びコンポーネントを作成し、ヒエラルキー上の任意のオブジェクトの階層下に設置します
+    /// </summary>
+    /// <typeparam name="T">作成するコンポーネントの型</typeparam>
+    /// <param name="initActive">ゲームオブジェクトの初期の有効・無効状態</param>
+    /// <returns>作成したコンポーネント</returns>
+    public T CreateComponentAndOrganizeWithDiContainer<T>(bool initActive, bool isBind, string objectName) where T : Behaviour
+    {
+        T generateCpt = _generator.InstantiateComponentWithDiContainerOnNewObj<T>(initActive, isBind, objectName);
+        NullCheck.AssertNotNull(generateCpt);
+
+        Organize(generateCpt);
+
+        return generateCpt;
+    }
+
+    /// <summary>
+    /// DIコンテナを用いてオブジェクト及びコンポーネントを作成し、ヒエラルキー上の任意のオブジェクトの階層下に設置します
+    /// </summary>
+    /// <typeparam name="T">作成するコンポーネントの型</typeparam>
+    /// <param name="initActive">ゲームオブジェクトの初期の有効・無効状態</param>
+    /// <returns>作成したコンポーネント</returns>
+    public T CreateAndAddComponentAndOrganizeWithDiContainer<T>(bool initActive, bool isBind, string objectName) where T : Behaviour
+    {
+        T generateCpt = _generator.InstantiateAndAddComponentWithDiContainer<T>(initActive, isBind, objectName);
+        NullCheck.AssertNotNull(generateCpt);
+
+        Organize(generateCpt);
+
+        return generateCpt;
+    }
+
+    /// <summary>
+    /// 引数に渡したオブジェクトからDIコンテナを用いてコンポーネントを作成し、ヒエラルキー上の任意のオブジェクトの階層下に設置します
+    /// </summary>
+    /// <typeparam name="T">作成するコンポーネントの型</typeparam>
+    /// <param name="gameObject">コンポーネントの元となるゲームオブジェクト</param>
+    /// <param name="initActive">ゲームオブジェクトの初期の有効・無効状態</param>
+    /// <param name="isBind">DIコンテナにバインドするか否か</param>
+    /// <returns>作成したコンポーネント</returns>
+    public T CreateComponentAndOrganizeWithDiContainer<T>(GameObject gameObject, bool initActive, bool isBind) where T : Behaviour
+    {
+        T generateCpt = _generator.InstantiateComponentWithDiContainer<T>(gameObject, initActive, isBind);
+        NullCheck.AssertNotNull(generateCpt);
+
+        Organize(generateCpt);
+
+        return generateCpt;
+    }
+
+    /// <summary>
+    /// 引数に渡したオブジェクトからDIコンテナを用いてコンポーネントを作成します
+    /// また、作成したコンポーネントを親とするオブジェクトの子として設定し、ヒエラルキー上の任意のオブジェクトの階層下に設置します
+    /// </summary>
+    /// <typeparam name="T">作成するコンポーネントの型</typeparam>
+    /// <param name="gameObject">コンポーネントの元となるゲームオブジェクト</param>
+    /// <param name="parentObject">ヒエラルキー上で作成したオブジェクトの親となるオブジェクト</param>
+    /// <param name="initActive">ゲームオブジェクトの初期の有効・無効状態</param>
+    /// <param name="isBind">DIコンテナにバインドするか否か</param>
+    /// <returns>作成したコンポーネント</returns>
+    public T CreateComponentNestedParentWithDiContainer<T>(GameObject gameObject, GameObject parentObject, bool initActive, bool isBind) where T : Behaviour
+    {
+        T generateCpt = _generator.InstantiateComponentWithDiContainer<T>(gameObject, initActive, isBind);
+        NullCheck.AssertNotNull(generateCpt);
+
+        generateCpt.transform.SetParent(parentObject.transform, false);
+
+        return generateCpt;
+    }
+
+    /// <summary>
+    /// 引数に渡したオブジェクトからDIコンテナを用いてコンポーネントを作成します
+    /// また、作成したコンポーネントを親とするオブジェクトの子として設定し、更に指定の名前で作成したディレクトリの子としてその親を設定の上、
+    /// ヒエラルキー上の任意のオブジェクトの階層下に設置します
+    /// </summary>
+    /// <typeparam name="T">作成するコンポーネントの型</typeparam>
+    /// <param name="gameObject">コンポーネントの元となるゲームオブジェクト</param>
+    /// <param name="parentObject">ヒエラルキー上で作成したオブジェクトの親となるオブジェクト</param>
+    /// <param name="newDirectoryObjectName">作成するヒエラルキー上のオブジェクト(ディレクトリの代替となる空オブジェクト)の名前</param>
+    /// <param name="initActive">ゲームオブジェクトの初期の有効・無効状態</param>
+    /// <returns>作成したコンポーネント</returns>
+    public T CreateComponentNestedNewDirectoryWithDiContainer<T>(GameObject gameObject, GameObject parentObject, string newDirectoryObjectName, bool initActive, bool isBind) where T : Behaviour
+    {
+        T generateCpt = _generator.InstantiateComponentWithDiContainer<T>(gameObject, initActive, isBind);
+        NullCheck.AssertNotNull(generateCpt);
+
+        GameObject folderObject = new GameObject(newDirectoryObjectName);
+        folderObject.transform.SetParent(parentObject.transform, false);
+        generateCpt.transform.SetParent(folderObject.transform, false);
+
+        return generateCpt;
+    }
+
+    /// <summary>
+    /// Diコンテナを用いてインスタンスを作成します
+    /// ヒエラルキー上には設置しません
+    /// </summary>
+    /// <typeparam name="T">作成するインスタンスの型</typeparam>
+    /// <param name="isBind">DIコンテナにバインドするか否か</param>
+    /// <returns>作成したインスタンス</returns>
+    public T InstantiateWithDiContainer<T>( bool isBind )
+    {
+        return _generator.InstantiateWithDiContainer<T>(isBind);
+    }
+
+    /// <summary>
+    /// 引数に指定したビヘイビアの紐づけ先のオブジェクトを決定します
+    /// </summary>
+    /// <param name="original">紐づけを行う対象オブジェクト</param>
+    /// <returns>紐づけ先のオブジェクト</returns>
+    virtual protected GameObject MapObjectToType<T>(T original)
+    {
+        return null;
+    }
+
+    /// <summary>
+    /// 生成されたオブジェクトを指定のオブジェクトの階層化に配置します
+    /// </summary>
+    /// <param name="bhv">生成されたオブジェクト</param>
+    private void Organize(Behaviour bhv)
+    {
+        GameObject parentObj = MapObjectToType(bhv);
+
+        if (parentObj != null)
+        {
+            bhv.transform.SetParent(parentObj.transform);
+        }
+    }
+}
