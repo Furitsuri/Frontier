768a009f0189d89d6c6c41d097176991bbbac505
Furitsuri <123718338+Furitsuri@users.noreply.github.com>
2025-07-28 22:21:58 +0900

    一部スクリプトのファイル名を変更

--- Assets/Game/Scripts/DebugTools/DebugEditorMonodriver.cs
+++ Assets/Game/Scripts/DebugTools/DebugEditorMonodriver.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using Zenject;
 
-public class DebugEditorMonoDriver : FocusRoutineBaseWithMonoBehaviour
+public class DebugEditorMonoDriver : FocusRoutineBase
 {
     private IEditorHandler _editorHnd;
 
