884053751fe0052b4ca293b0d4a507021f0c7ef6
Furitsuri <123718338+Furitsuri@users.noreply.github.com>
2025-08-09 12:06:50 +0900

    int型インスタンスなどの範囲判定関数を追加

---
+++ Assets/Game/Scripts/Utility/NumericExtensions.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+public static class NumericExtensions
+{
+    public static bool IsBetween<T>(this T value, T min, T max) where T : IComparable<T>
+    {
+        return value.CompareTo(min) >= 0 && value.CompareTo(max) <= 0;
+    }
+}
