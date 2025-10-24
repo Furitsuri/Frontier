df0ede5dbdce33164aacf835758b3151569dad02
Furitsuri <123718338+Furitsuri@users.noreply.github.com>
2025-10-24 14:54:31 +0900

    フェーズアニメーション用のStateの追加

---
+++ Assets/Game/Scripts/StateMachine/Phase/EmPhaseAnimationState.cs
@@ -0,0 +1,14 @@
+﻿using Frontier.Battle;
+
+namespace Frontier.StateMachine
+{
+    public class EmPhaseStateAnimation : PhaseAnimationStateBase
+    {
+        override protected void StartPhaseAnim()
+        {
+            _btlUi.TogglePhaseUI( true, TurnType.ENEMY_TURN );
+
+            base.StartPhaseAnim();
+        }
+    }
+}
---
+++ Assets/Game/Scripts/StateMachine/Phase/PlPhaseAnimationState.cs
@@ -0,0 +1,14 @@
+﻿using Frontier.Battle;
+
+namespace Frontier.StateMachine
+{
+    public class PlPhaseStateAnimation : PhaseAnimationStateBase
+    {
+        override protected void StartPhaseAnim()
+        {
+            _btlUi.TogglePhaseUI( true, TurnType.PLAYER_TURN );
+
+            base.StartPhaseAnim();
+        }
+    }
+}
