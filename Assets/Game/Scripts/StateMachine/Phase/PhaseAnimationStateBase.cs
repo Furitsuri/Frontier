0bb0812066a6753e3ac04404c90066764ac60a45
Furitsuri <123718338+Furitsuri@users.noreply.github.com>
2025-10-24 14:54:31 +0900

    フェーズアニメーション用のStateの追加

---
+++ Assets/Game/Scripts/StateMachine/Phase/PhaseAnimationStateBase.cs
@@ -0,0 +1,45 @@
+﻿using Frontier.Battle;
+using Zenject;
+
+namespace Frontier.StateMachine
+{
+    public class PhaseAnimationStateBase : PhaseStateBase
+    {
+        private enum TRANSIT_TAG
+        {
+            NONE = -1,
+            SELECT_TILE,
+
+            NUM,
+        }
+
+        [Inject] protected BattleUISystem _btlUi = null;
+
+        /// <summary>
+        /// フェーズアニメーションを再生します
+        /// </summary>
+        virtual protected void StartPhaseAnim()
+        {
+            _btlUi.StartAnimPhaseUI();
+        }
+
+        override public void Init()
+        {
+            StartPhaseAnim();   // フェーズアニメーションの開始
+        }
+
+        override public bool Update()
+        {
+            // フェーズアニメーション中は操作無効
+            if( _btlUi.IsPlayingPhaseUI() )
+            {
+                return false;
+            }
+
+            // フェーズアニメーションが終了すれば自動的にPlSelectTileへ遷移
+            TransitIndex = ( int ) TRANSIT_TAG.SELECT_TILE;
+
+            return base.Update();
+        }
+    }
+}
