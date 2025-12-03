af214067eef5df46ff36a6643c101bb19dfd48ba
Furitsuri <123718338+Furitsuri@users.noreply.github.com>
2025-12-03 21:40:40 +0900

    ステージエディタのファイル名入力の補間処理部分を修正

--- Assets/Game/Scripts/Utility/File/FileSuggestor.cs
+++ Assets/Game/Scripts/Utility/File/FileSuggestor.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -10,26 +11,62 @@ public class FileSuggestor : MonoBehaviour
     [SerializeField] private GameObject suggestionItemPrefab;
     [SerializeField] private string targetDirectory = "C:/Users/tssm4/AppData/LocalLow/DefaultCompany/FRONTIER/StageData";
 
-    private void Start()
+    private int _defaultChildCount = 0;
+    private Action OnPressedTabAction = null;
+
+    private void Awake()
     {
-        // fileInputField.onValueChanged.AddListener( OnTextChanged );
+        _defaultChildCount = suggestionParent.childCount;
+    }
+
+    void Update()
+    {
+        if( Input.GetKeyDown( KeyCode.Tab ) )
+        {
+            OnPressedTabAction?.Invoke();
+        }
+    }
+
+    public void Init( Action onPressedTab )
+    {
+        OnPressedTabAction = onPressedTab;
+
+        gameObject.SetActive( false );
     }
 
     public void StartSuggest()
     {
+        gameObject.SetActive( true );
+
         fileInputField.onValueChanged.AddListener( OnTextChanged );
+    }
+
+    public void EndSuggest()
+    {
+        DestroyTextMeshChild();
+
+        gameObject.SetActive( false );
+    }
+
+    /// <summary>
+    /// 候補の最上位のGameObjectを取得します
+    /// </summary>
+    /// <returns></returns>
+    public GameObject GetTopMostSuggestion()
+    {
+        GameObject[] textMeshChilds = GetTextMeshChildArray();
+
+        if( textMeshChilds.Length == 0 ) { return null; }
+        return textMeshChilds[0];
     }
 
     private void OnTextChanged( string text )
     {
         // 既存の候補をクリア
-        foreach( Transform child in suggestionParent )
-        {
-            Destroy( child.gameObject );
-        }
+        DestroyTextMeshChild();
 
-        if( string.IsNullOrEmpty( text ) ) return;
-        if( !Directory.Exists( targetDirectory ) ) return;
+        if( string.IsNullOrEmpty( text ) ) { return; }
+        if( !Directory.Exists( targetDirectory ) ) { return; }
 
         var files = Directory.GetFiles( targetDirectory );
 
@@ -38,18 +75,36 @@ public class FileSuggestor : MonoBehaviour
             string fileName = Path.GetFileName( path );
 
             // 入力に合致したファイルのみ表示
-            if( !fileName.ToLower().Contains( text.ToLower() ) ) continue;
+            if( !fileName.ToLower().Contains( text.ToLower() ) ) { continue; }
 
             var item = Instantiate( suggestionItemPrefab, suggestionParent );
-            var tmp = item.GetComponentInChildren<Text>();
+            item.gameObject.SetActive( true );
+            var tmp = item.GetComponentInChildren<TextMeshProUGUI>();
             tmp.text = fileName;
-
-            // 候補をクリック → InputField に反映
-            var button = item.GetComponent<Button>();
-            button.onClick.AddListener( () =>
-            {
-                fileInputField.text = fileName;
-            } );
         }
+    }
+
+    private void DestroyTextMeshChild()
+    {
+        foreach( Transform child in suggestionParent )
+        {
+            if( null != child.GetComponentInChildren<TextMeshProUGUI>() )
+            {
+                Destroy( child.gameObject );
+            }
+        }
+    }
+
+    private GameObject[] GetTextMeshChildArray()
+    {
+        var list = new System.Collections.Generic.List<GameObject>();
+        foreach( Transform child in suggestionParent )
+        {
+            if( null != child.GetComponentInChildren<TextMeshProUGUI>() )
+            {
+                list.Add( child.gameObject );
+            }
+        }
+        return list.ToArray();
     }
 }
