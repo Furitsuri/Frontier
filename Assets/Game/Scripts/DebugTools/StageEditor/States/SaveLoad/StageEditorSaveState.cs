c06d2ac84bb5beb126f380a854fa97c8b1d56986
Furitsuri <123718338+Furitsuri@users.noreply.github.com>
2025-12-04 17:33:49 +0900

    ステージ編集のファイルロード時に失敗した場合の挙動が作成されていなかったため修正

--- Assets/Game/Scripts/DebugTools/StageEditor/States/SaveLoad/StageEditorSaveState.cs
+++ Assets/Game/Scripts/DebugTools/StageEditor/States/SaveLoad/StageEditorSaveState.cs
@@ -6,6 +6,7 @@
         {
             _confirmMessage[( int ) State.CONFIRM]  = "SAVE THIS STAGE?";
             _confirmMessage[( int ) State.NOTIFY]   = "STAGE SAVED";
+            _failedMessage                          = "FAILED TO SAVE STAGE";
 
             base.Init();
         }
