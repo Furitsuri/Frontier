c2850b4f705251b27769208e6c4e32f9a9e8579a
Furitsuri <123718338+Furitsuri@users.noreply.github.com>
2026-05-24 01:02:20 +0900

    コメントの編集 攻撃対象範囲がPartOfRange時における戦闘結果の予測表示の挿入

--- Assets/Game/Scripts/Entities/Characters/ActionRange/PartOfRangeTargetingRange.cs
+++ Assets/Game/Scripts/Entities/Characters/ActionRange/PartOfRangeTargetingRange.cs
@@ -66,36 +66,6 @@ namespace Frontier.Entities
             var actionRangeCtrl     = context.Owner.BattleLogic.ActionRangeCtrl;
             attackTargetCharaKeys   = actionRangeCtrl.GetAttackTargetCharacterKeys();
 
-
-            /*
-            // 攻撃可能なグリッド内に敵がおり、尚且つ現在のターゲットキャラクターが存在しない場合は、ターゲットキャラクターを設定する
-            if( 0 < attackTargetCharaKeys.Count )
-            {
-                if( null == targetCharacter )
-                {
-                    targetCharacter = context.BtlRtnCtrl.BtlCharaCdr.GetNearestCharacter( context.Owner, attackTargetCharaKeys );
-                    Debug.Assert( targetCharacter != null, "攻撃可能なグリッドが存在する場合、必ずターゲットキャラクターが存在するはずですが、nullが返されました。" );
-                    context.StageCtrl.MoveGridCursorToAttackableTile( targetCharacter );
-                }
-                else
-                {
-                    // 現在のターゲットキャラクターが攻撃可能なグリッド内にいる場合は、ターゲットキャラクターを再設定しない
-                    if(!attackTargetCharaKeys.Contains( targetCharacter.GetCharacterKey() ))
-                    {
-                        targetCharacter = context.BtlRtnCtrl.BtlCharaCdr.GetNearestCharacter( context.Owner, attackTargetCharaKeys );
-                        Debug.Assert( targetCharacter != null, "攻撃可能なグリッドが存在する場合、必ずターゲットキャラクターが存在するはずですが、nullが返されました。" );
-                        context.StageCtrl.MoveGridCursorToAttackableTile( targetCharacter );
-                    }
-                }
-            }
-            else
-            {
-                targetCharacter = null;
-            }
-
-            context.Presenter.SetActiveActionResultExpect( targetCharacter != null, ParameterWindowType.Left );
-            */
-
             // 攻撃可能なキャラクターが攻撃対象範囲に居る場合、ターゲット選択時グリッドカーソルをそのキャラクターに合わせる
             if( 0 < attackTargetCharaKeys.Count )
             {
@@ -120,6 +90,8 @@ namespace Frontier.Entities
                 targetCharacter = null;
                 context.StageCtrl.SetActiveTargetCursor( false );
             }
+
+            context.Presenter.SetActiveActionResultExpect( targetCharacter != null, ParameterWindowType.Left );
         }
 
 		static public bool TryAdjustRange( TargetingRangeContext context, int step, ref int currentRange, int maxRange, bool isMovingSkill, ref List<CharacterKey> attackTargets, ref Character targetCharacter )
