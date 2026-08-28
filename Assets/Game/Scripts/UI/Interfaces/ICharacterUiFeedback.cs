7d7d6b1435db6943eb5a95446214be130376bbcd
Furitsuri <123718338+Furitsuri@users.noreply.github.com>
2026-08-28 11:54:15 +0900

    キャラクター関連のUI表示周りのインターフェースを用意

---
+++ Assets/Game/Scripts/UI/Interfaces/ICharacterUiFeedback.cs
@@ -0,0 +1,16 @@
+﻿using Frontier;
+using Frontier.Entities;
+
+/// <summary>
+/// 戦闘中のキャラクター自身に紐づくUI演出(ダメージ表記・HPゲージ・スキルボックスの明滅停止等)を
+/// 呼び出すための限定インターフェースです。
+/// Character・BattleLogicBase・BattleAnimationEventReceiverのような戦闘エンティティ層のクラスに、
+/// IUiSystem全体(RecruitUiやDeployUi等、無関係な画面まで)を見せないようにする目的で用意しています。
+/// </summary>
+public interface ICharacterUiFeedback
+{
+    void ShowDamageOnCharacter( Character chara, float duration = -1f );
+    void ShowHpGaugeOnCharacter( Character chara );
+    void RemoveHpGaugeOnCharacter( Character chara );
+    SkillBoxUI GetPlayerParamSkillBox( int index );
+}
